789d7825cbb42b70f3ca5c14ed4db6328e53e8cc
desiBoy <desiboy9012@gmail.com>
2024-11-14 14:29:38 -0600

    membership banner fixed

--- src/pages/Membership.js
+++ src/pages/Membership.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import bannerImage from '../assets/images/purpleBanner.png';
+import bannerImage from '../assets/images/membershipBanner.png';
 import '../assets/fonts/Poppins-Regular.ttf';
 
 const Membership = () => (
