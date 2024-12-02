1140600e39d89837deaa324b9a05986fb80089f0
Safee36 <muhammadsafee5643@gmail.com>
2024-12-02 23:35:13 +0500

    terms page

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -33,7 +33,7 @@ const Terms = () => (
         sx={{
           color: "#3D3D3C",
           fontFamily: "Caprasimo",
-          fontSize: { xs: "20px", sm: "32px", md: "40px" },
+          fontSize: { xs: "32px", md: "40px" },
           position: "absolute",
           bottom: "10%",
           fontWeight: 400,
