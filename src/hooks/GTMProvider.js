148961263c194239486e3448b296905f5fff08ca
fatima-codes0 <kirvehari98@gmail.com>
2025-04-05 04:44:32 +0500

    profile page

--- src/hooks/GTMProvider.js
+++ src/hooks/GTMProvider.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
 
  const GTMProvider = ({ children }) => {
-  const GTM_ID = "GTM-KSZWJ7P3";
+  const GTM_ID =  process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID ?? "GTM-KSZWJ7P3";
   
   
   useEffect(() => {
